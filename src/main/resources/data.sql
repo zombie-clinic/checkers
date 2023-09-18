a8387f33318f6808b89e893caa9de7c4fc5ce60a
Podarkes <yevgen.gryaznov@gmail.com>
2023-09-18 22:43:58 +0200

    Add in-memory database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,15 @@
+--CREATE TABLE game (
+--    id UUID PRIMARY KEY,
+--    state VARCHAR(255)
+--);
+
+INSERT into game (id, state) values ('0a83d555-abfe-42f9-8d46-0d23d7a1d863', 'starting');
+INSERT into game (id, state) values ('1f3bea4e-48e3-4b7b-8ff8-6287f201b510', 'starting');
+INSERT into game (id, state) values ('7304942c-1bfd-4c23-8c83-c9902a866807', 'ongoing');
+INSERT into game (id, state) values ('cb3e0675-3689-4d7f-b5d6-1f3c19b657c9', 'ongoing');
+INSERT into game (id, state) values ('5525d529-9bb5-489a-b61b-2397962f9d6e', 'completed');
+INSERT into game (id, state) values ('0e59c559-b0ef-4345-8c05-40a267b8a8a3', 'completed');
+INSERT into game (id, state) values ('79d401ab-ac28-4f27-8c42-ee907c886d74', 'completed');
+INSERT into game (id, state) values ('0f71e103-2278-4a75-b5c0-e555f859fdac', 'completed');
+INSERT into game (id, state) values ('12157ca4-fb32-4b66-baa8-c73c54593fcd', 'deleted');
+INSERT into game (id, state) values ('4a639f32-7555-4a7d-82a7-c3a8613f5b7f', 'deleted');
