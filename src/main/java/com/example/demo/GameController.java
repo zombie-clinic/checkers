1f243fd5b43c7cc1faf8dde737a1db6fad569935
Podarkes <yevgen.gryaznov@gmail.com>
2023-10-07 22:02:42 +0200

    Display a board

--- src/main/java/com/example/demo/GameController.java
+++ src/main/java/com/example/demo/GameController.java
@@ -1,26 +1,20 @@
 package com.example.demo;
 
-import io.swagger.v3.oas.annotations.Hidden;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@Hidden
-@RestController
+@Controller
 public class GameController {
 
     @GetMapping("/")
-    @ResponseBody
-    public ResponseEntity<?> index() {
-        Map<String, String> response = new HashMap<>();
-        response.put("choice1", "white");
-        response.put("choice2", "black");
-        response.put("choice3", "random");
-        response.put("button", "start");
-        return ResponseEntity.ok(response);
+    public String index(Model model) {
+        boolean[][] myArray = {
+                {true, false, true},
+                {false, true, false},
+                {true, false, true}
+        };
+        model.addAttribute("myArray", myArray);
+        return "index";
     }
 }
