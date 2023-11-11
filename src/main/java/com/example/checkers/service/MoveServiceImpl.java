f30729b6dac4f1081c92abc3876644e6d70a95e1
Podarkes <yevgen.gryaznov@gmail.com>
2023-11-12 00:32:52 +0100

    Fix a bug which produced an extra piece

--- src/main/java/com/example/checkers/service/MoveServiceImpl.java
+++ src/main/java/com/example/checkers/service/MoveServiceImpl.java
@@ -49,7 +49,7 @@ public class MoveServiceImpl implements MoveService {
             moveRequest.getState().getDark().remove(Integer.valueOf(split[0]));
             moveRequest.getState().getDark().add(Integer.valueOf(split[1]));
 
-            Move move = new Move(game, player, "BLACK", moveRequest.getMove(),
+            Move move = new Move(game, player, "LIGHT", moveRequest.getMove(),
                     moveRequest.getState().getDark().stream().map(String::valueOf).collect(Collectors.joining(",")),
                     moveRequest.getState().getLight().stream().map(String::valueOf).collect(Collectors.joining(",")));
 
