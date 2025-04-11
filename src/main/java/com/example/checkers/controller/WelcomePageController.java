6c14bd94ac16dde434fb15a65ee50838e179ce7a
Podarkes <yevgen.gryaznov@gmail.com>
2025-04-11 21:44:13 +0200

    refactor

--- src/main/java/com/example/checkers/controller/WelcomePageController.java
+++ src/main/java/com/example/checkers/controller/WelcomePageController.java
@@ -1,13 +1,10 @@
-package com.example.checkers;
+package com.example.checkers.controller;
 
 import io.swagger.v3.oas.annotations.Hidden;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Hidden
 @RestController
