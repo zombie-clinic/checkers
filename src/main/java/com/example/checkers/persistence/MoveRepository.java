b431326eae0b97fbe11dc375b8acecb9b04cdb2f
Yevgen <yevgen.gryaznov@gmail.com>
2025-06-08 00:09:37 +0200

    feat: migrate to Maven

--- src/main/java/com/example/checkers/persistence/MoveRepository.java
+++ src/main/java/com/example/checkers/persistence/MoveRepository.java
@@ -3,9 +3,9 @@ package com.example.checkers.persistence;
 import com.example.checkers.domain.Move;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource(exported = false)
+@Repository
 public interface MoveRepository extends JpaRepository<Move, String> {
 
   List<Move> findAllByGameId(String id);
